f3dc5950beb7006cb562328f6c52b25a0af2a315
ale-dg <ale.20.09@hotmail.com>
2024-04-10 15:06:22 -0600

    push example

---
+++ sql_exercices/case.sql
@@ -0,0 +1,17 @@
+SELECT COUNT(job_id) AS number_of_jobs,
+    CASE
+        WHEN job_location = 'Anywhere' THEN 'Remote'
+        WHEN job_location = 'New York, NY' THEN 'Local'
+        ELSE 'Onsite'
+    END AS location_category
+FROM job_postings_fact
+WHERE job_title = 'Data Analyst'
+GROUP BY location_category;
+/*
+ 
+ Label new column as follows:
+ - 'Anywhere' jobs as 'Remote'
+ - 'New York, NY' as 'Local'
+ - 'Otherwise 'Onsite
+ 
+ */
